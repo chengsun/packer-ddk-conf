b90f19bbaacdc8f20c709a325845b2b1e97001cc
Cheng Sun <cheng.sun@citrix.com>
2014-12-11 15:00:20 +0000

    Add logging messages

--- ddk-build.sh
+++ ddk-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# setup host for NAT
+echo "### Setting up host NAT"
 
 expect -c "spawn ssh $HOST_USERNAME@$HOST_IP" \
 	   -c "expect -exact \"$HOST_USERNAME@$HOST_IP's password: \"" \
@@ -22,19 +22,25 @@ EOF
 rpmurl='http://www.uk.xensource.com/carbon/trunk-c7/xe-phase-1-latest/binary-packages/RPMS/domain0/RPMS'
 packer_dir='/local/scratch/packer'
 
+echo "### Downloading XenServer RPMs"
+
 mkdir -p rpms
 pushd rpms
 wget -c -r --no-directories --no-parent -R 'index.html,kernel-debuginfo-*,kernel-modules-*' -A 'kernel-*,xe-guest-utilities-*' "$rpmurl/x86_64/"
 wget -c -r --no-directories --no-parent -R 'index.html' -A 'supp-pack-build-*,xcp-python-libs-*,xenserver-ddk-files-*' "$rpmurl/noarch/"
 popd
 
+echo "### Starting Packer"
+
 PACKER_LOG=1 exec $packer_dir/packer build ddk.conf 2>&1 &
 packer_pid=$!
 
 sigterm_handler() {
-	echo "Caught SIGTERM."
+	echo "### Caught SIGTERM."
 	kill -INT $packer_pid 2>/dev/null
 }
 
 trap sigterm_handler TERM
 wait $packer_pid
+
+echo "### Done."
